f5f4a1782ea17800858bffb9e63bb93b3c816ea2
Stepan Ananin <xrf848@gmail.com>
2026-02-16 20:52:09 +0300

    upd github.com/abaxoth0/Ain dependecy

--- go.mod
+++ go.mod
@@ -2,10 +2,12 @@ module arcturus
 
 go 1.24.0
 
-require github.com/abaxoth0/Ain v1.1.0
+require (
+	github.com/abaxoth0/Ain v1.2.0
+	github.com/json-iterator/go v1.1.12
+)
 
 require (
-	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 )
--- packages/shared/logger/logger.go
+++ packages/shared/logger/logger.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/abaxoth0/Ain/logger"
+	jsoniter "github.com/json-iterator/go"
 )
 
 var (
@@ -12,9 +13,33 @@ var (
 	Stderr = logger.NewStdErrLogger("")
 )
 
+type serializer struct {
+	*jsoniter.Stream
+}
+
+func newSerializer() *serializer {
+	return &serializer{
+		Stream: jsoniter.NewStream(jsoniter.ConfigFastest, nil, 1024),
+	}
+}
+
+func (s *serializer) Reset() {
+	s.Stream.Reset(nil)
+}
+
+func (s *serializer) WriteVal(v any) error {
+	s.Stream.WriteVal(v)
+	return nil
+}
+
+func (s *serializer) Buffer() []byte {
+	return s.Stream.Buffer()
+}
+
 var DefaultConfig = &logger.FileLoggerConfig{
 	Path:     "/tmp/arcturus",
-	FilePerm: 0744,
+	FilePerm: 0644,
+	SerializerProducer: func() logger.Serializer { return newSerializer() },
 	LoggerConfig: &logger.LoggerConfig{
 		ApplicationName: "arcturus",
 	},
